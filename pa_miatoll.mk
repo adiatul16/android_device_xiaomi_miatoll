165b94241bf332a13ed4a0a8e4075479f7f6eeea
adiatul16 <adiatul16@gmail.com>
2021-05-07 15:56:20 +0530

    miaoll: Stay Paranoid

--- pa_miatoll.mk
+++ pa_miatoll.mk
@@ -8,13 +8,13 @@
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
 # Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/pa/config/common_full_phone.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := lineage_miatoll
+PRODUCT_NAME := pa_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Miatoll

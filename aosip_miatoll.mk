84e81cbdfe936bb9dd944b07da6ef08b39f46f8a
adiatul16 <adiatul16@gmail.com>
2021-05-11 23:52:20 +0530

    miatoll: Adapt for AOSip

--- aosip_miatoll.mk
+++ aosip_miatoll.mk
@@ -7,14 +7,14 @@
 # Inherit from miatoll  device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSip stuff
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := lineage_miatoll
+PRODUCT_NAME := aosip_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Miatoll

6ddca46704a0b904cc3c8af28a702aece72073e2
=adiatul16 <=adiatul16@gmail.com>
2022-07-04 10:47:44 +0530

    miatoll: Adapt for CrDroid

--- lineage_miatoll.mk
+++ lineage_miatoll.mk
@@ -8,8 +8,7 @@
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
 # Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
-$(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
 
 # Quick Tap
 TARGET_SUPPORTS_QUICK_TAP := true
@@ -18,16 +17,13 @@ TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := arrow_miatoll
+PRODUCT_NAME := lineage_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Miatoll
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Maintainer
-DEVICE_MAINTAINER := SonalSingh18
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
